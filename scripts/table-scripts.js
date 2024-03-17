f07c45eddd42fcd20662cc57f63e7e4b35070028
Alexander Petrenko <dradgo@gmail.com>
2024-03-17 12:43:15 +0300

    - handle extra contacts added

--- scripts/table-scripts.js
+++ scripts/table-scripts.js
@@ -12,16 +12,10 @@ function nameFormatter(value, row, index) {
 
 function showExtractContact(personId) {
     let person = getPerson(personId);
-    const contactLimit = 5;
-    if(person.contacts.length > contactLimit) {
-        let restContacts = person.contacts.length - contactLimit;
-        let title = "";
-        for (let index = contactLimit; index < person.contacts.length; index++) {
-            let contact = person.contacts[index];
-            title += `**${contact.type}**: ${contact.value} \n`;
-        }
-        alert(title);
-    }
+    $('#rest-contacts-' + personId).removeAttr('hidden');
+    const $extractContactButton = $('#extra-contact-' + personId);
+    $extractContactButton.prop('hidden', 'hidden');
+    $extractContactButton.attr('hidden', 'hidden');
     return true;
 }
 
@@ -36,17 +30,23 @@ function contactFormatter(value, row, index) {
         contactItems.push(itemHtml);
     }
     if(person.contacts.length > contactLimit) {
-        let restContacts = person.contacts.length - contactLimit;
+        const restContacts = person.contacts.length - contactLimit;
         if(restContacts === 1) {
             contactItems.push(convertContactToHtml(person.contacts[contactLimit]));
         } else {
+            let restContactItems = [];
+            restContactItems.push(`<span id="rest-contacts-${person.id}" hidden="hidden">`);
             let title = "";
             for (let index = contactLimit; index < person.contacts.length; index++) {
                 let contact = person.contacts[index];
+                const itemHtml = convertContactToHtml(contact);
+                restContactItems.push(itemHtml);
                 title += `**${contact.type}**: ${contact.value} \n`;
             }
             const itemHtml = `<span id="extra-contact-${person.id}" class="fa fa-plus" onclick="showExtractContact(${person.id})">${restContacts}</span>`;
             contactItems.push(itemHtml);
+            restContactItems.push(`</span>`);
+            restContactItems.forEach(c => contactItems.push(c));
         }
 
     }
